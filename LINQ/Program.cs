ee73a1edfc0497eec212bce1413a6341ae5d0a03
SoapyEnigma <keithlekgolo@gmail.com>
2025-07-26 12:04:14 -0400

    Update Program.cs

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> games = new List<string>()
+            {
+                "The Legend of Zelda: Ocarina of Time",
+                "Tetris",
+                "Super Mario 64",
+                "The Last of Us",
+                "Red Dead Redemption 2",
+                "Grand Theft Auto V",
+                "The Witcher 3: Wild Hunt",
+                "Half-Life 2",
+                "Elden Ring",
+                "Resident Evil 4"
+            };
+
+            games.OrderBy(x => x).ToList().ForEach(Console.WriteLine);
         }
     }
 }
